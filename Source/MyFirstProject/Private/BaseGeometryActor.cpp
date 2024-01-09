4e438c2650ed36fddb9322804741f58f62cb252c
Vyacheslav Maslov <maslovslava007@gmail.com>
2024-01-09 21:11:41 +0300

    lesson 6 - FTransform

--- Source/MyFirstProject/Private/BaseGeometryActor.cpp
+++ Source/MyFirstProject/Private/BaseGeometryActor.cpp
@@ -11,13 +11,18 @@ ABaseGeometryActor::ABaseGeometryActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>("BaseMesh");
+	SetRootComponent(BaseMesh);
 }
 
 // Called when the game starts or when spawned
 void ABaseGeometryActor::BeginPlay()
 {
 	Super::BeginPlay();
-	printTypes();
+	
+	InitialLocation = GetActorLocation();
+
+	//printTypes();
 }
 
 // Called every frame
@@ -25,6 +30,11 @@ void ABaseGeometryActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FVector CurrentLocation = GetActorLocation();
+	float time = GetWorld()->GetTimeSeconds();
+	CurrentLocation.Z = InitialLocation.Z + Aplitude * FMath::Sin(Frequancy * time);
+
+	SetActorLocation(CurrentLocation);
 }
 
 void ABaseGeometryActor::printTypes()
@@ -54,5 +64,21 @@ void ABaseGeometryActor::printStringTypes()
 
 	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, Name);
 	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, Stat, true, FVector2D(1.5f, 1.5f));
+}
+
+void ABaseGeometryActor::printTranformString()
+{
+	FTransform Transform = GetActorTransform();
+	FVector Location = Transform.GetLocation();
+	FRotator Rotator = Transform.Rotator();
+	FVector Scale = Transform.GetScale3D();
+
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Actor name %s"), *GetName());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Transform %s"), *Transform.ToString());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Location %s"), *Location.ToString());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Rotator %s"), *Rotator.ToString());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Scale %s"), *Scale.ToString());
+
+	UE_LOG(LogBaseGeometry, Error, TEXT("Human Transform %s"), *Transform.ToHumanReadableString());
 }
 
